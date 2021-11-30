c3afaa0971cb9d07143e7c469af8dcfd222636dc
vargasmiguel <54147317+vargasmiguel@users.noreply.github.com>
2021-11-30 10:32:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,17 @@ def prices(day: str):
         return {'error': 'No data for this day'}
     else:
         return {'arabica-dura':data[2], 'arabica-rio':data[4], 'conilon':data[7]}
+    
+@app.get("/month")
+def month():
+    req = urllib.request.Request(f'https://www.cccv.org.br/cotacao/', headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'})
+    page = urllib.request.urlopen(req)
+    charset=page.info().get_content_charset()
+    content=page.read().decode(charset)
+    bs = BeautifulSoup(content)
+    data = bs.findAll(lambda tag: tag.name=='td')
+    data=[x.text for x in data]
+    resp={}
+    for i in range(0, len(data2), 4):
+        resp[data2[i]] = {'arabica-dura':data2[i+1], 'arabica-rio':data2[i+2], 'conilon':data2[i+3]}
+    return resp
